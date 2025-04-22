e54249b2c474d2f17bcd46943bd4f29127ec4e70
nrusimha17 <nrusimha17@gmail.com>
2025-04-22 15:55:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ tweet_chain = tweet_prompt | gemini_model
 
 import streamlit as st
 
-st.header("Tweet Generator - NRU")
+st.header("BOOK REVIEW")
 
 st.subheader("Generate tweets using Generative AI")
 
